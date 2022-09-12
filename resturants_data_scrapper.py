ae7f61c936ea48de3c185b878abc75c198af7979
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-12 23:41:58 +0530

    replaced the response type

--- resturants_data_scrapper.py
+++ resturants_data_scrapper.py
@@ -6,7 +6,7 @@ url = "https://www.zomato.com/jabalpur/restaurants"
 user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 
 webpage = r.get(url, headers= user_agent)
-soup = bs(webpage.content, "html.parser")
+soup = bs(webpage.text, "html.parser")
 
 name=[]
 cuisine= []
