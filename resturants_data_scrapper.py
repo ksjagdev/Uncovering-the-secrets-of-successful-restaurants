556e02a29615d4aa0c5715d00fd2cbfaad132e97
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-12 22:32:19 +0530

    added user_agent for gaining permission to scrape

--- resturants_data_scrapper.py
+++ resturants_data_scrapper.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 
 url = "https://www.zomato.com/jabalpur/restaurants"
+user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 
-webpage = r.get(url)
+webpage = r.get(url, headers= user_agent)
 soup = bs(webpage.content, "html.parser")
-print(soup)
+
