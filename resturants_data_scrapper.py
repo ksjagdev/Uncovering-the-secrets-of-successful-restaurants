42f59a64b506a3ba18a9a513bddcab58eb0c3ebc
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-13 00:01:54 +0530

    updated .gitignore

--- resturants_data_scrapper.py
+++ resturants_data_scrapper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 from path import driver_path
 from selenium import webdriver
+import time
 
 url = "https://www.zomato.com/jabalpur/restaurants"
 user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
