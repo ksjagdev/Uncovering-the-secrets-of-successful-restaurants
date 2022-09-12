0e3c39d5c7d217ca1b3e62d59ccd01f12cdd6b94
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-12 22:30:19 +0530

    added url and created a bs object from the url

--- resturants_data_scrapper.py
+++ resturants_data_scrapper.py
@@ -1,3 +1,9 @@
 import requests as r
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+
+url = "https://www.zomato.com/jabalpur/restaurants"
+
+webpage = r.get(url)
+soup = bs(webpage.content, "html.parser")
+print(soup)
