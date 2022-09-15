48775385f70ce6a8c8a4faa9f18cc132ffe6ec57
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-15 22:57:29 +0530

    created a function to extract and import data

--- restaurants_data_scrapper.py
+++ restaurants_data_scrapper.py
@@ -10,17 +10,6 @@ homepage_url = "https://www.zomato.com/jabalpur/restaurants"
 user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 rest_homepage_url = "https://www.zomato.com/"
 
-
-homepage_soup = bs(driver.page_source, "html.parser")
-
-rest_names=[]
-cuisines= []
-rating= []
-price_per_person = []
-locality = []
-all_rest_page_url = []
-
-name_tags = homepage_soup.find_all("h4")
 def browser_driver(scroll=False):
     options = webdriver.EdgeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -45,39 +34,57 @@ def browser_driver(scroll=False):
                 break
     return driver
 
+def data_extraction():
+    rest_names=[]
+    cuisines= []
+    rating= []
+    price_per_person = []
+    locality = []
+    all_rest_page_url = []
 
-for rest_name in name_tags[:len(name_tags)-1]:
+    homepage_soup = bs(browser_driver().page_source, "html.parser")
 
-    rest_names.append(rest_name.text)
+    name_tags = homepage_soup.find_all("h4")
 
-    rest_page_tag = rest_name.parent.parent 
+    for rest_name in name_tags[:len(name_tags)-1]:
 
-    rest_page_tag = rest_page_tag["href"][:-5] + "reviews"
-    all_rest_page_url.append(rest_page_tag)
+        rest_names.append(rest_name.text)
 
-    rating_tag = rest_name.parent.div.div.div.div.div.div.text
-    rating.append(rating_tag)
+        rest_page_tag = rest_name.parent.parent 
 
-    price_tag = rest_name.parent.next_sibling.p.next_sibling.text
-    price_per_person.append(price_tag)
+        rest_page_tag = rest_page_tag["href"][:-5] + "reviews"
+        all_rest_page_url.append(rest_page_tag)
 
-    cuisine_tag = rest_name.parent.next_sibling.p.text
-    cuisines.append(cuisine_tag)
+        rating_tag = rest_name.parent.div.div.div.div.div.div.text
+        rating.append(rating_tag)
+
+        price_tag = rest_name.parent.next_sibling.p.next_sibling.text
+        price_per_person.append(price_tag)
+
+        cuisine_tag = rest_name.parent.next_sibling.p.text
+        cuisines.append(cuisine_tag)
+    browser_driver().close()
+
+    for page_tags in all_rest_page_url:
+        page_url = rest_homepage_url + page_tags
+        browser_driver.get(page_url)
+        
+        rest_page_soup = bs(browser_driver.page_source, "html.parser")
+
+        h1_tag= rest_page_soup.find_all("h1")
+        
+        location_tag = h1_tag[1].parent.next_sibling.div.next_sibling.text
+        locality.append(location_tag)
+
+    def data_export(name):
+        name = input("Save the exported file as: ")
+        restaurants_df = pd.DataFrame({"name": rest_names, "cuisines": cuisines, "rating": rating, "price_per_person": price_per_person, "location": locality})
+        restaurants_df.to_csv(f"./Dataset/{name}.csv")
+    data_export()
+
+data_extraction()
 
 
-for page_tags in all_rest_page_url:
-    page_url = rest_homepage_url + page_tags
-    driver.get(page_url)
-    
-    rest_page_soup = bs(driver.page_source, "html.parser")
 
-    h1_tag= rest_page_soup.find_all("h1")
-    
-    location_tag = h1_tag[1].parent.next_sibling.div.next_sibling.text
-    locality.append(location_tag)
 
-driver.close()
-
-restaurants_df = pd.DataFrame({"name": rest_names, "cuisines": cuisines, "rating": rating, "price_per_person": price_per_person, "location": locality})
-restaurants_df.to_csv("./Dataset/jabalpur_restaurants.csv")
 
