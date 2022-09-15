2e0548bb87bbba6d658cb36599c21cfaa1ec620e
Kunwardeep Singh Jagdev <67104287+Kunwardeep-Singh-Jagdev@users.noreply.github.com>
2022-09-15 22:54:28 +0530

    created and implemented a function to control browser

--- restaurants_data_scrapper.py
+++ restaurants_data_scrapper.py
@@ -8,27 +8,8 @@ check_dir()  # This function is responsible for checking and changing the workin
 
 homepage_url = "https://www.zomato.com/jabalpur/restaurants"
 user_agent = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+rest_homepage_url = "https://www.zomato.com/"
 
-options = webdriver.EdgeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Edge(executable_path=driver_path, options= options)
-driver.get(homepage_url)
-
-time.sleep(1)  # Suspends the webpage for 1 seconds
-scroll_pause_time = 2  # Time interval between two consecutive scrolls
-screen_height = driver.execute_script("return window.screen.height;")  # extract the screen height of the webpage
-i = 1
-
-while True:
-    # scroll a height of one screen at a time
-    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
-    i += 1
-    time.sleep(scroll_pause_time)
-    # update scroll height after each scroll
-    scroll_height = driver.execute_script("return document.body.scrollHeight;")
-    # Break the loop when the height to scroll to is larger than the total scroll height
-    if (screen_height) * i > scroll_height:
-        break
 
 homepage_soup = bs(driver.page_source, "html.parser")
 
@@ -40,8 +21,29 @@ locality = []
 all_rest_page_url = []
 
 name_tags = homepage_soup.find_all("h4")
+def browser_driver(scroll=False):
+    options = webdriver.EdgeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Edge(executable_path=driver_path, options= options)
+    driver.get(homepage_url)
+    
+    if scroll == True:
+        time.sleep(1)  # Suspends the webpage for 1 seconds
+        scroll_pause_time = 2  # Time interval between two consecutive scrolls
+        screen_height = driver.execute_script("return window.screen.height;")  # extract the screen height of the webpage
+        i = 1
 
-rest_homepage_url = "https://www.zomato.com/"
+        while True:
+            # scroll a height of one screen at a time
+            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+            i += 1
+            time.sleep(scroll_pause_time)
+            # update scroll height after each scroll
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")
+            # Break the loop when the height to scroll to is larger than the total scroll height
+            if (screen_height) * i > scroll_height:
+                break
+    return driver
 
 
 for rest_name in name_tags[:len(name_tags)-1]:
